26c4f5a69899800bd723a824863f4f04d2b72ec9
md mahadi hasan <mahadihasan625601hstu@gmail.com>
2024-01-04 07:06:56 +0600

    practice of html css js

---
+++ power.js
@@ -0,0 +1,13 @@
+//document.getElementById('play').innerHTML="12312";
+
+//document.getElementById("play").innerText = "I have changed!";
+console.log("mahadi hasan");
+
+
+
+let element=document.getElementById("play");
+console.log(element);
+document.getElementsByClassName('work').style.color="red";
+
+
+classElement.innerText="Welcome to happy new year 2024";
